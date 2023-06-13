885f7247580a5aaf2465c192302adeb51b5912b4
Sampa  Rozario <srozario22@gmail.com>
2023-06-13 16:21:10 +0200

    academic semester added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Application } from 'express'
 import express from 'express'
 import cors from 'cors'
 import globalErrorHandeler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/user/user.route'
+import routes from './app/routes'
 
 const app: Application = express()
 
@@ -11,7 +11,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/v1/users', UserRoutes)
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1/', routes)
 
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   Promise.reject(new Error('Unhandles Promise Rejection'))
