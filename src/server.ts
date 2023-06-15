a159f574f6822bd19008e10eccfa6f78ac7fe189
Sampa  Rozario <srozario22@gmail.com>
2023-06-15 08:58:42 +0200

    student created

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-import { logger, errorlogger } from './shared/logger'
-import { Server } from 'http'
+import { errorlogger, logger } from './shared/logger'
 
 process.on('uncaughtException', error => {
   errorlogger.error(error)
   process.exit(1)
 })
+
 let server: Server
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    logger.info('Database connected successfully')
+    logger.info(`🛢   Database is connected successfully`)
 
     server = app.listen(config.port, () => {
-      logger.info(`Application is listening on port ${config.port}`)
+      logger.info(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
-    errorlogger.error('failed to connect server', err)
+    errorlogger.error('Failed to connect database', err)
   }
 
   process.on('unhandledRejection', error => {
-    // console.log('error detected')
     if (server) {
       server.close(() => {
         errorlogger.error(error)
@@ -34,6 +34,7 @@ async function bootstrap() {
     }
   })
 }
+
 bootstrap()
 
 process.on('SIGTERM', () => {
